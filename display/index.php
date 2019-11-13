2706a4533aa50618ba0db8c4b8c58f6701526b6d
unknown <ThisIsMattSaunders>
2019-11-14 12:24:19 +1300

    annoyingly broke responsivness, but started to add in student pages

--- display/index.php
+++ display/index.php
@@ -19,6 +19,22 @@ require 'includes/statement.php';
 <body>
 
 	<div class="container-fluid">
+
+		<div class="studentHero col-12 colsml-21 colmd-12 collrg-12 colxlrg-12">
+
+		<div class="justify-content">
+			<img class="portrait" src="img/vin.jpg">
+		</div>
+
+		<div class="content">
+			<h3>Vin Diesel</h3>
+			<p><i>You know this ain't no 10-second race</i></p>
+		</div>
+
+
+		</div>
+
+
 		<div class="container-row">
 			<div class="colmd-1 collrg-1 colxlrg-2"></div>
 			<div class="landing col-12 colmd-10 collrg-10 colxlrg-8">
@@ -32,7 +48,7 @@ require 'includes/statement.php';
 
 	<div class="container-fluid">
 		<div class="container-row">
-			<div class="justify-content col-12 colsml-6 colmd-4 collrg-4 colxlrg-4">
+			<div class="justify-content col-12 colsml-6 colmd-3 collrg-3 colxlrg-3">
 				<div class="graphic card container-row">
 					<div class="col-4">
 						<img class="card-img" src="img/vin.jpg">
@@ -46,7 +62,7 @@ require 'includes/statement.php';
 					</div>
 				</div>
 			</div>
-			<div class="justify-content col-12 colsml-6 colmd-4 collrg-4 colxlrg-4">
+			<div class="justify-content col-12 colsml-6 colmd-3 collrg-3 colxlrg-3">
 				<div class="web card container-row">
 					<div class="col-4">
 						<img class="card-img" src="img/vin.jpg">
@@ -60,7 +76,7 @@ require 'includes/statement.php';
 					</div>
 				</div>
 			</div>
-			<div class="justify-content col-12 colsml-6 colmd-4 collrg-4 colxlrg-4">
+			<div class="justify-content col-12 colsml-6 colmd-3 collrg-3 colxlrg-3">
 				<div class="gameArt card container-row">
 					<div class="col-4">
 						<img class="card-img" src="img/vin.jpg">
@@ -77,6 +93,19 @@ require 'includes/statement.php';
 		</div>
 	</div>
 
+	<div class="studentPage">
+		<div class="container-row">
+			<div class="portraitLrg col-4 colsml-4 colmd-4 collrg-4 colxlrg-4">
+				<img src="img/vin.jpg">
+			</div>
+			<div class="bioLrg col-8 colsml-8 colmd-8 collrg-8 colxlrg-8">
+				<h2> Big Daddy Diesel </h2>
+				<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+			</div>
+		</div>
+	</div>
+
+	<script src="node_modules/jquery/dist/jquery.js"></script>
 	<script src="js/main.min.js"></script>
 </body>
 </html>
--- display/js/main.js
+++ display/js/main.js
@@ -1,1 +1,15 @@
 console.log('JS has loaded');
+
+
+$(".graphic").click(function(){
+  console.log('click');
+if( $(".studentHero").is( ":hidden" ) ){
+  $(".studentHero").slideDown(800);
+  $(".studentPage").slideUp(800);
+} else {
+  $(".studentHero").slideDown(800);
+  $(".studentPage").slideUp(800);
+}
+
+
+});
